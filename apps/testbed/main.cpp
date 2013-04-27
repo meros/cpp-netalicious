aa9026c8d230c847dea1fdff20f59f807cc607db
Alexander Schrab <meros@Alexanders-MacBook-Air.local>
2013-04-27 12:40:38 +0200

    gtest

--- apps/testbed/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "test2.h"
-
-using namespace std;
-
-int main (int argc, char** argv) {
-  cout << "Testing123" << endl;
-  testfunction2();
-}
---
+++ libs/netalicious/gtests/test_class_loop.cpp
@@ -0,0 +1,15 @@
+// SubtractTest.cpp
+#include "gtest/gtest.h"
+#include "loop.hpp"
+
+
+TEST(LoopTest, SubtractTwoNumbers)
+{
+    boost::shared_ptr<netalicious::Loop> loop (new netalicious::Loop);
+}
+
+TEST(LoopTest, SubtractTwoNumbers2)
+{
+    std::cout << "asdf" << std::endl;
+    boost::shared_ptr<netalicious::Loop> loop (new netalicious::Loop);
+}
